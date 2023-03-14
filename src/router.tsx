ec51b75e12a052b9b76c24ac2e3c192ca04f1c03
BaeJoonSoo <baejoonsoo21@gmail.com>
2023-03-14 12:12:04 +0900

    INIT: project setting
    - 불필요한 기본 파일 제거
    - 모듈 설치
        - axios
        - react-router-dom
        - recoil
        - styled-components
    - 폴더구조 설정
    - tsconfig baseUrl 설정

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+const Router = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
