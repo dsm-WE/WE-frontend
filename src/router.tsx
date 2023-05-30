f0345dc4239b52be20ef8e18bd1b2d48c14f131e
ohjuhye <bbangbbang562@gmail.com>
2023-05-30 10:15:19 +0900

    feat: router

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyPage from 'pages/myPage';
 import Main from 'pages/main';
-import Auth from 'pages/auth';
 import UserEdit from 'pages/myPage/edit';
 import WritePost from 'pages/portfolio/write';
 import PortfolioDetail from 'pages/portfolio/detail/[id]';
 import Header from 'components/common/header';
 import ScrollToTop from 'components/ScrollTop';
+import Login from 'pages/login';
+import Signup from 'pages/signup';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route element={<Header />}>
           <Route path="/" element={<Main />} />
           <Route path="/myPage">
